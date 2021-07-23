71eadeeb6e5afd09709620ee875b9def3fddd8dd
riccjohn <riccjohn@gmail.com>
2021-07-23 15:54:25 -0500

    WIP: Chop.guess

---
+++ lib/6-Modules-Named-Functions/Modules_Functions-6.exs
@@ -0,0 +1,10 @@
+defmodule Chop do
+  def guess(actual, range do
+    # guess between a low number and a high -> guess middle number
+    # tell user our guess
+    # "yes" -> game over
+    # "bigger" -> bigger(low, high)
+    # "smaller" -> smaller(low, high)
+    # anything else -> tell user to enter a valid response
+  end
+end
